d6c3cadb5c4fb7d0ebc406f8e88a3a1fa966bcf2
Danny <fisher.d88@gmail.com>
2023-02-21 20:01:06 -0700

    Add function to clear canvas.
    Add event listener to clear button to call function to clear canvas.

--- draw.js
+++ draw.js
@@ -1,15 +1,23 @@
 const ui = {
-    canvas : document.querySelector('.canvas')
+    canvas : document.querySelector('.canvas'),
+    clear : document.querySelector('.clear'),
 };
 
-ui.canvas.style.display = 'grid';
-ui.canvas.style['grid-template-columns'] = 'repeat(16, auto)';
+function clearCanvas(){
+    ui.allCells.forEach((element) => {element.style['background-color'] = 'white'});
+}
 
-for(let i = 0; i< 16; i++){
-    for(let j = 0; j < 16; j++){
+ui.canvas.style.display = 'grid';
+ui.canvas.style['grid-template-columns'] = 'repeat(128, auto)';
+
+for(let i = 0; i< 128; i++){
+    for(let j = 0; j < 128; j++){
         let cell = document.createElement('div');
         cell.classList.add('cell');
         cell.addEventListener('mouseover', (e) => { e.target.style['background-color'] = 'black';});
         ui.canvas.appendChild(cell);
     }
 }
+
+ui.allCells = document.querySelectorAll('.cell');
+ui.clear.addEventListener('click', clearCanvas);
