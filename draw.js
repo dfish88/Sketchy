5dcb3a59c581a55207d9fd1e760c238c615f75a8
Danny <fisher.d88@gmail.com>
2023-02-22 14:29:20 -0700

    Add function to generate random color for rainbow.

--- draw.js
+++ draw.js
@@ -2,7 +2,8 @@ const ui = {
     canvas : document.querySelector('.canvas'),
     clearButton : document.querySelector('.clear'),
     sizeSelector : document.querySelector('.size-selector'),
-    colorSelector : document.querySelector('.color-selector')
+    colorSelector : document.querySelector('.color-selector'),
+    rainbowToggle : document.querySelector('.rainbow-toggle')
 };
 
 function removeCells(){
@@ -15,6 +16,29 @@ function clearCanvas(){
     ui.allCells.forEach((element) => {element.style['background-color'] = 'white'});
 }
 
+function getRandomColor(){
+    const colors = {
+        1 : 'rgba(255, 0, 0, 1)',
+        2 : 'rgba(0, 255, 0, 1)',
+        3 : 'rgba(0, 0, 255, 1)',
+        4 : 'rgba(255, 154, 0, 1)',
+        5 : 'rgba(208, 222, 33, 1)',
+        6 : 'rgba(79, 220, 74, 1)',
+        7 : 'rgba(63, 218, 216, 1)',
+        8 : 'rgba(47, 201, 226, 1)',
+        9 : 'rgba(28, 127, 238, 1)',
+        10 : 'rgba(251, 7, 217, 1)',
+        11 : 'rgba(255, 220, 33, 1)',
+    }
+    return colors[Math.floor(Math.random() * Object.keys(colors).length) + 1];
+}
+
+function getColor(){
+    if (ui.rainbowToggle.classList.contains('active'))
+        return getRandomColor()
+    return ui.colorSelector.value;
+}
+
 function buildCanvas(size){
     ui.canvas.style.display = 'grid';
     ui.canvas.style['grid-template-columns'] = `repeat(${size}, auto)`;
@@ -22,7 +46,7 @@ function buildCanvas(size){
         for(let j = 0; j < size; j++){
             let cell = document.createElement('div');
             cell.classList.add('cell');
-            cell.addEventListener('mouseover', (e) => { e.target.style['background-color'] = ui.colorSelector.value;});
+            cell.addEventListener('mouseover', (e) => { e.target.style['background-color'] = getColor();});
             ui.canvas.appendChild(cell);
         }
     }
@@ -32,6 +56,7 @@ function buildCanvas(size){
 function setUp(){
     ui.clearButton.addEventListener('click', clearCanvas);
     ui.sizeSelector.addEventListener('change', (e) => { removeCells(); buildCanvas(e.target.value);})
+    ui.rainbowToggle.addEventListener('click', () => {ui.rainbowToggle.classList.toggle('active')})
     buildCanvas(ui.sizeSelector.value);
 }
 
